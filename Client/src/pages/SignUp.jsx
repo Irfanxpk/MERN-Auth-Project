392614d896f2bd40e2c63f47c90ae5ac89830cb7
Irfan PK <pkirfanx@gmail.com>
2024-05-13 21:02:59 +0530

    Completed Sign-up page functionality

--- Client/src/pages/SignUp.jsx
+++ Client/src/pages/SignUp.jsx
@@ -1,32 +1,74 @@
-import {Link } from 'react-router-dom'
+
+import { useState } from "react";
+import { Link } from "react-router-dom";
 const SignUp = () => {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+    
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try
+    {
+      setLoading(false);
+
+      setError(false)
+
+      const res = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+      if (data.success == false)
+      {
+        setError(true)
+        return
+      }
+
+    } catch (err){
+      setLoading(false)
+      setError(err)
+    }
+  }
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
-      <form className="flex flex-col gap-4">
+
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
         <input
           type="text"
           className="bg-slate-100 p-3 rounded-lg"
           id="username"
           placeholder="Username"
+          onChange={handleChange}
         />
         <input
           type="email"
           className="bg-slate-100 p-3 rounded-lg"
           id="email"
           placeholder="Email"
+          onChange={handleChange}
         />
         <input
           type="password"
           className="bg-slate-100 p-3 rounded-lg"
           id="password"
           placeholder="Password"
+          onChange={handleChange}
         />
-        <button
+        <button disabled={loading}
           className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
-          type="button"
-        >
-          Sign up
+          type="submit"
+        >{
+          loading ? 'Loading.......':'Sign up'}
         </button>
       </form>
       <div className="flex gap-2 mt-5 ">
@@ -35,8 +77,9 @@ const SignUp = () => {
           <span className="text-blue-500"> Sign-in</span>
         </Link>
       </div>
+      <p className="text-red-700 mt-5 ">{error && 'Somthing went wrong!..'}</p>
     </div>
   );
-}
+};
 
-export default SignUp
+export default SignUp;
