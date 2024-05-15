9b5f0b6b5d78a6de430f95ed260e073eddab1aea
Irfan PK <pkirfanx@gmail.com>
2024-05-15 21:17:04 +0530

    completed home and removed about page

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import About from "./pages/About";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Profile from "./pages/Profile";
@@ -14,7 +13,6 @@ const App = () => {
       <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route element={<PrivateRoute/> }>
--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -1,7 +1,12 @@
+import { useSelector } from "react-redux";
 
 const Home = () => {
+  const { currentUser,  } = useSelector((state) => state.user);
+  console.log(currentUser)
   return (
-    <div>Home</div>
+    <div className="flex justify-center">
+      <h1 className=" text-3xl font-bold mb-4 text-slate-800">Welcome {currentUser ? currentUser.username : ""} </h1>
+    </div>
   )
 }
 
